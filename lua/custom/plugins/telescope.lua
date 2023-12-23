6e0a976b1a70f046e297a9a6896c58c009a24340
Mohammad Aljafar <xqg1122@gmail.com>
2023-12-23 23:44:10 +0300

    commit

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,11 +1,18 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons"
+    },
     config = function()
         local builtin = require("telescope.builtin")
 
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
         vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
         vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+
+        require("telescope").load_extension("fzf")
     end
 }

0b0511c1edac669f0b0eb659c666cbf196db1be4
mohammad_alj <xqg1122@gmail.com>
2023-12-27 21:32:00 +0300

    I did something idk

--- lua/custom/core/set.lua
+++ lua/custom/core/set.lua
@@ -3,11 +3,13 @@ vim.g.mapleader = " "
 -- Line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
 -- Tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 
 vim.opt.smartindent = true
 

d94a5e841c62edeaf7ac1864d5e57330ed065fec
mohammad_alj <xqg1122@gmail.com>
2023-12-29 01:18:51 +0300

    A CoMMITMkm???

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -30,6 +30,7 @@ return {
             lspconfig.pyright.setup({})
             lspconfig.emmet_language_server.setup({})
 
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})

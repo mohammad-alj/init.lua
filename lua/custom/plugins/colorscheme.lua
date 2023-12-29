56b4139acf8d8d74514363c69a63e5d985f9b8f2
Mohammad Aljafar <xqg1122@gmail.com>
2023-12-30 00:20:00 +0300

    ye

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,7 +1,3 @@
-return {
-    "olimorris/onedarkpro.nvim", name = "onedark", priority = 1000,
-    config = function() 
-        vim.cmd('colorscheme onedark')
-    end
-}
-
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function()
+    vim.cmd('colorscheme catppuccin "mocha"')
+end }

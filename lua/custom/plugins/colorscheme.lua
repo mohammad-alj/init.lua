08ea9f8063ede4ab2723f3337ea73140f3a049b6
Mohammad Aljafar <xqg1122@gmail.com>
2024-01-09 04:48:41 +0300

    Change the theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,3 +1,6 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function()
-    vim.cmd('colorscheme catppuccin "mocha"')
-end }
+return {
+    'navarasu/onedark.nvim',
+    config = function()
+        require('onedark').load()
+    end
+}
